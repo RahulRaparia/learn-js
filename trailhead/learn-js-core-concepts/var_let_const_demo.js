1d77a61197f858436edbd64cccac2a3fe82bfb3a
rahulraparia-ac <rahulraparia.cs@gmail.com>
2023-08-10 16:34:21 +0530

    trailhead-js-learning

---
+++ trailhead/learn-js-core-concepts/var_let_const_demo.js
@@ -0,0 +1,20 @@
+//Primitive Assignment
+
+var myBike = "Mountain Bike"; 
+
+let currentGear = 5; 
+
+const numberOfGears = 12; 
+
+//reassignment
+
+myBike = "Penny Farthing"; // works
+
+currentGear = 1; // works
+
+// numberOfGears = 1; //error 
+
+// print the values 
+console.log(`My Bike var : ${myBike}`);
+console.log(`Current Gear let : ${currentGear}`);
+console.log(`Number of Gears const : ${numberOfGears}`);
